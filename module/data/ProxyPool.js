e424fafd6399d151af5b2b9c3a2f2941a106b9fd
carlansades <m15868095558@163.com>
2018-04-11 15:52:32 +0800

    update

--- module/data/ProxyPool.js
+++ module/data/ProxyPool.js
@@ -4,78 +4,81 @@
 var _list = [];
 var _hash = {};
 class ProxyPool {
-	constructor()
-	{
+	constructor() {
 
 	}
 
-	update($list)
-	{
-		for (var item of $list)
-		{
+	update($list) {
+		for (var item of $list) {
 			this.add(item);
 		}
 	}
 
-	add($item)
-	{
+	add($item) {
 		var itemData = createData($item);
-		if (!_hash[itemData.id])
-		{
+		if (!_hash[itemData.id]) {
 			_hash[itemData.id] = itemData;
 			_list.push(itemData);
 		}
 	}
 
-	remove($id)
-	{
+	remove($id) {
 		var index = _list.indexOf(_hash[$id]);
-		if (index >= 0)
-		{
+		if (index >= 0) {
 			_list.splice(index, 1)
 		}
 	}
 
-	getDataById($id)
-	{
+	getDataById($id) {
 		return _hash[$id]
 	}
 
-	get list()
-	{
+	get list() {
 		return _list;
 	}
 
-	removeAll()
-	{
+	removeAll() {
 		_list = [];
 		_hash = {};
 	}
 }
 
-function createData($dObj)
-{
+function createData($dObj) {
 	var d = {};
 	d.id = Math.random();
 	d.ip = "";
 	d.port = "";
 	d.address = "";
 	d.protocol = "";
-	d.validtime = "";
+	d.validTime = "";
 	d.createTime = "";
+	d.complete = "";
 	d.update = updateData.bind(d);
 	d.update($dObj)
 	return d;
 }
 
-function updateData($dObj)
-{
+function updateData($dObj) {
 	$dObj.hasOwnProperty("ip") && (this.ip = $dObj.ip);
 	$dObj.hasOwnProperty("port") && (this.port = $dObj.port);
 	$dObj.hasOwnProperty("address") && (this.address = $dObj.address);
-	$dObj.hasOwnProperty("protocol") && (this.protocol = $dObj.protocol);
-	$dObj.hasOwnProperty("validtime") && (this.validtime = $dObj.validtime);
+	$dObj.hasOwnProperty("protocol") && (this.protocol = $dObj.protocol.toLowerCase());
 	$dObj.hasOwnProperty("createTime") && (this.createTime = $dObj.createTime);
+
+	if ($dObj.hasOwnProperty("validTime")) {
+		if ($dObj.validTime.indexOf("天") >= 0) {
+			var index = $dObj.validTime.indexOf("天");
+			$dObj.validTime = $dObj.validTime.substr(0, index)
+			$dObj.validTime = $dObj.validTime * 24;
+		} else if ($dObj.validTime.indexOf("小时") >= 0) {
+			var index = $dObj.validTime.indexOf("小时");
+			$dObj.validTime = $dObj.validTime.substr(0, index)
+		} else {
+			$dObj.validTime = 0;
+		}
+		this.validTime = $dObj.validTime
+	};
+	this.complete = this.protocol + "://" + this.ip + ":" + this.port;
 }
 
 module.exports = ProxyPool;
--- module/proxy/proxy.js
+++ module/proxy/proxy.js
@@ -7,8 +7,12 @@ var superagent = require('../libs/superagent');
 var dataPool = require('../data/DataPool')
 var cheerio = require('cheerio');
 var config = require('../config/config.ganji.js')
+var {
+	convertArray
+} = require('./../util/index')
 var page = 1;
-
+var timerId = null;
+var $;
 module.exports = class {
 	constructor() {
 		this.add('init', this.init);
@@ -16,13 +20,44 @@ module.exports = class {
 	}
 
 	init($data, $succcess, $error, $client) {
-		checkProxy(dataPool.proxyPool.list)
+		this.getProxyList();
 	}
+
 	getProxyList() {
 		superagent.get(config.proxyServer + page, {}).then((response) => {
-			$ = cheerio.load(response.text);
+			if (response) {
+				g.fs.writeFile("log/test.log", JSON.stringify(response))
+				$ = cheerio.load(response.text);
+				this.cleanoutData($("tbody tr"))
+				timerId = setTimeout(() => {
+					page++;
+					this.getProxyList();
+				}, 10000)
+			} else {
+				var list = dataPool.proxyPool.list;
+				this.checkProxy(list)
+				clearTimeout(timerId);
+			}
 		});
+
+
 	}
+
+	cleanoutData($nodeList) {
+		var nodeList = convertArray($nodeList)
+		for (var node of nodeList) {
+			var item = {};
+			var children = $(node).children();
+			item.ip = children.eq(1).text()
+			item.port = children.eq(2).text()
+			item.address = children.eq(3).children().first().text()
+			item.protocol = children.eq(5).text()
+			item.validTime = children.eq(8).text()
+			item.createTime = children.eq(9).text()
+			dataPool.proxyPool.add(item);
+		}
+	}
+
 	checkProxy(proxyList) {
 		var targetOptions = {
 			method: 'GET',
@@ -37,12 +72,13 @@ module.exports = class {
 
 		function sliceProxy() {
 			if (proxys.length > 0) {
-				var proxy = "http://" + proxys.shift();
+				var proxyItem = proxys.shift()
+				var proxy = proxyItem.complete.indexOf("http") < 0 ? "http://" + proxyItem.complete : proxyItem.complete;
+				trace("proxy", proxy)
 				superagent.get(targetOptions.url, {
 					proxy: proxy
 				}).then((response) => {
 					sliceProxy();
-					// 				g.fs.writeFile("data.log", JSON.stringify(response))
 					if (response.status == 200) {
 						validProxys.push(proxy);
 						trace(`验证成功==>> ${proxy}`);
@@ -50,7 +86,9 @@ module.exports = class {
 					}
 				});
 			} else {
-				g.fs.writeFile("result.log", JSON.stringify(validProxys))
+				g.fs.writeFile("result.log", JSON.stringify(validProxys), {
+					flag: "a"
+				})
 			}
 		}
 	}
